3de0bb1b9051853695d16a68bdd8468949c8d89c
example <alisayar0107@gmail.com>
2021-04-22 12:55:57 +0300

    updateCountdown function created to submit button and take data from form.

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+// Global variables for title and date
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set date input min with today's date
 
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+}
+
+// Event listeners
+countdownForm.addEventListener('submit', updateCountdown);
