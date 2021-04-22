85f0cbe3fa391804e39446b37c7b06e856cae1b4
example <alisayar0107@gmail.com>
2021-04-22 13:17:01 +0300

    countdown constants created to display results

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 // Global variables for title and date
 let countdownTitle = '';
 let countdownDate = '';
